34fc299b5d79e4777d87cb6be65bc1d8935a98e3
Graham Campbell <graham@bugsnag.com>
2016-07-16 20:00:44 +0100

    Fixed daemon calls

--- dogegen.py
+++ dogegen.py
@@ -16,10 +16,9 @@ def main(args):
         args (list[str]): List of command line arguments.
     """
     if args[1] == "--daemon-start":
-        dogegen.make_daemon()
+        DogeDaemon.make_daemon()
     elif args[1] == "--with-daemon":
-        doge_daemon = dogegen.get_daemon(args[2])
-        doge_daemon.make_meme(args[3], args[4], int(args[5]))
+        DogeDaemon.get_daemon(args[2]).make_meme(args[3], args[4], int(args[5]))
     else:
         DogeGen().make_meme(args[1], args[2], int(args[3]))
 
